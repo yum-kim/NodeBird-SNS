ff3e9b4c2ab882671cd9dac63bef3f1bf552fccd
YumiKim <dwkimym93@gmail.com>
2022-09-19 23:30:36 +0900

    💄 antd module import

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import propTypes from 'prop-types';
 import Link from 'next/link';
+import { Menu } from 'antd';
 
-const AppLayout = ({children}) => {
+const AppLayout = ({ children }) => {
+    const items = [
+        {
+            label: <Link href="/"><a>메인</a></Link>,
+            key: 'main'
+        },
+        {
+            label: <Link href="/profile"><a>프로필</a></Link>,
+            key: 'profile'
+        },
+        {
+            label: <Link href="/signup"><a>회원가입</a></Link>,
+            key: 'signup'
+        }
+    ];
+
     return (
         <div>
-            <div>
-                <Link href="/"><a>메인</a></Link>
-                <Link href="/profile"><a>프로필</a></Link>
-                <Link href="/signup"><a>회원가입</a></Link>
-            </div>
+            <Menu mode="horizontal" items={items} />
             {children}
         </div>
     )
