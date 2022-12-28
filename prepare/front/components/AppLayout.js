0e91ddcf3dc1f01110f0028d672fbd1e39e1c62f
YumiKim <dwkimym93@gmail.com>
2022-12-28 23:33:48 +0900

    imageZoom 컴포넌트에서 스크롤 생기는 오류 수정 (gutter)

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -2,13 +2,28 @@ import React from 'react';
 import propTypes from 'prop-types';
 import Link from 'next/link';
 import { Menu, Input, Row, Col } from 'antd';
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import { useSelector } from 'react-redux';
 import UserProfile from './UserProfile';
 import LoginForm from './LoginForm';
 
 const SearchInput = styled(Input.Search)`
   vertical-align: middle;  
+`;
+
+const Global = createGlobalStyle`
+    .ant-row {
+        margin-right: 0 !important;
+        margin-left: 0 !important;
+    }
+
+    .ant-col:first-child {
+        padding-left: 0 !important;
+    }
+
+    .ant-col:last-child {
+        padding-right: 0 !important;
+    }
 `;
 
 const AppLayout = ({ children }) => {
@@ -34,6 +49,7 @@ const AppLayout = ({ children }) => {
 
     return (
         <div>
+            <Global />
             <Menu mode="horizontal" items={items} />
             <Row gutter={8}>
                 <Col xs={24} md={6}>
