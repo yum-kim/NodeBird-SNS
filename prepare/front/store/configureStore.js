fa5d4bb4825c140ba4284b140ea75e201bf40973
YumiKim <dwkimym93@gmail.com>
2023-01-30 21:52:13 +0900

    redux-saga 모듈 추가

--- prepare/front/store/configureStore.js
+++ prepare/front/store/configureStore.js
@@ -1,14 +1,22 @@
 import { createWrapper } from 'next-redux-wrapper';
 import { createStore, compose, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import rootSaga from 'redux-saga';
 import reducer from '../reducers';
 
+const loggerMiddleware = ({ dispatch, getState }) => (next) => (action) => {
+    console.log(action);
+    return next(action);
+}
+
 const configureStore = () => {
-    const middlewares = [];
+    const sagaMiddelware = createSagaMiddleWare();
+    const middlewares = [sagaMiddelware];
     const enhancer = process.env.NODE_ENV === 'production'
         ? compose(applyMiddleware(...middlewares)) //배포용 (devtool 연결 X)
         : composeWithDevTools(applyMiddleware(...middlewares)) //개발용
     const store = createStore(reducer, enhancer);
+    store.sagaTask = sagaMiddelware.run(rootSaga);
     return store;
 };
 
