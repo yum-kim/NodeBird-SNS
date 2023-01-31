ea6b947fbccf3ebe9681792117089c228e9fc317
YumiKim <dwkimym93@gmail.com>
2023-01-31 10:39:47 +0900

    reducer 주석 추가

--- prepare/front/reducers/index.js
+++ prepare/front/reducers/index.js
@@ -8,6 +8,8 @@ import { combineReducers } from 'redux';
  *  -> 초기 state가 있고, action을 받아서 다음 state가 만들어짐
  * initialState랑은 객체가 다름 (그래야 prev, next state 둘 다 기록에 남음)
  */
+
+//combineReducers -> 여러 reducer를 사용할 경우 reducer를 하나로 묶어주는 메소드 (store에 저장되는 reducer는 1개)
 const rootReducer = combineReducers({
     index: (state = {}, action) => {
         switch (action.type) {
