7e67f3d81c43a84999750d98c5d6f35f88769632
YumiKim <dwkimym93@gmail.com>
2023-02-07 16:17:27 +0900

    redux store 오류 수정 (rootSaga import 경로)

--- prepare/front/store/configureStore.js
+++ prepare/front/store/configureStore.js
@@ -1,8 +1,9 @@
 import { createWrapper } from 'next-redux-wrapper';
 import { createStore, compose, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import rootSaga from 'redux-saga';
+import createSagaMiddleware from 'redux-saga';
 import reducer from '../reducers';
+import rootSaga from '../pages/sagas';
 
 const loggerMiddleware = ({ dispatch, getState }) => (next) => (action) => {
     console.log(action);
@@ -10,8 +11,8 @@ const loggerMiddleware = ({ dispatch, getState }) => (next) => (action) => {
 }
 
 const configureStore = () => {
-    const sagaMiddelware = createSagaMiddleWare();
-    const middlewares = [sagaMiddelware];
+    const sagaMiddelware = createSagaMiddleware();
+    const middlewares = [sagaMiddelware, loggerMiddleware];
     const enhancer = process.env.NODE_ENV === 'production'
         ? compose(applyMiddleware(...middlewares)) //배포용 (devtool 연결 X)
         : composeWithDevTools(applyMiddleware(...middlewares)) //개발용
