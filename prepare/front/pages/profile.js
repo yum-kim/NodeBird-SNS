a3c3ef4fd92c0d08c06edc0726f45f31a60829ef
YumiKim <dwkimym93@gmail.com>
2023-03-08 17:18:46 +0900

    로그아웃 상태에서 프로필 페이지 접근 시 오류 수정

--- prepare/front/pages/profile.js
+++ prepare/front/pages/profile.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Head from 'next/head';
+import Router from 'next/router';
 import AppLayout from '../components/AppLayout';
 import NicknameEditForm from '../components/NicknameEditForm';
 import FollowList from '../components/FollowList';
@@ -7,6 +8,16 @@ import { useSelector } from 'react-redux';
 
 const Profile = () => {
     const { me } = useSelector((state) => state.user);
+
+    useEffect(() => {
+        if (!(me && me.id)) {
+            Router.push('/');
+        }
+    }, [me && me.id])
+
+    if (!me) {
+        return null;
+    }
 
     return (
         <>
